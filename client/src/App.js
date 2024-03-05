bd399f39aa48a297d027742ea7e3dfec586c72ca
Saif Pirani <pirani@saifs-mbp.lan>
2024-03-05 01:11:22 -0600

    First Commit

---
+++ client/src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Home } from "./pages/home";
+import { Login } from "./pages/login";
+import { Register } from "./pages/register";
+
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
