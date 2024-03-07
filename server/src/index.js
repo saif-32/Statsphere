bf34dcfbc5df5d681192f1954d46df894a38f9af
elizabethreyes2022 <112584418+elizabethreyes2022@users.noreply.github.com>
2024-03-07 15:59:04 -0600

    Update index.js

--- server/src/index.js
+++ server/src/index.js
@@ -7,5 +7,6 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+mongoose.connect("mongodb+srv://statsspheretech:Hannon10!@stats.s4pqakt.mongodb.net/stats?retryWrites=true&w=majority&appName=stats");
 
 app.listen(3001, () => console.log("Server is running."))
