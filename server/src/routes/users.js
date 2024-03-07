abdd2c22a4a2c949257d8989edb968c369284e4b
elizabethreyes2022 <112584418+elizabethreyes2022@users.noreply.github.com>
2024-03-07 17:08:23 -0600

    Update users.js

--- server/src/routes/users.js
+++ server/src/routes/users.js
@@ -1,7 +1,18 @@
 import express from 'express'
 import jwt from 'jsonwebtoken' 
-import bcypt from 'byrypt' 
+import bcypt from 'byrypt'
+import { UserModel } from "../models/Users.js";
 
-const router = express.Router() 
+const router = express.Router();
+
+router.post("/register", async ( req, res) => {
+  const { username, password } = req.body; 
+
+  const user = await UserModel.findOne({username: username});
+
+  res.json(user);
+});
+
+router.post("login")
 
 express { router as userRouter };
