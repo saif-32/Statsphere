17ce60c35d114c9bebcd5b6db16958ded25a389c
elizabethreyes2022 <112584418+elizabethreyes2022@users.noreply.github.com>
2024-04-14 14:38:50 -0500

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,13 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Home } from "./pages/home";
 import { Login } from "./pages/login";
 import { Register } from "./pages/register";
-
+import {Navbar} from "./components/navbar";
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <Navbar />
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/login' element={<Login />} />
